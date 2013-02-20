55862971492c829387a4fe1f79a062a1b1d1bd6c
William Henney <will@henney.org>
2013-02-20 17:54:26 -0600

    First working version of extract-orders.py
    This writes a separate FITS image for each order, and another FITS image
    of the wavelengths, which are averaged to give a function of the x-axis
    only.

--- extract-orders.py
+++ extract-orders.py
@@ -4,37 +4,24 @@ import pyregion
 import argparse
 import scipy.ndimage as ndi
 
+# Mapping between the boxes that we added by hand and the labels of the contiguous patches
+box2label = {
+    "51" : 24, "52" : 23, "53" : 22, "54" : 21, "55" : 20, "56" : 19, "57" : 18, 
+    "58" : 17, "59" : 16, "60" : 15, "61" : 14, "62" : 14, "63" : 14, "64" : 13, 
+    "65" : 12, "66" : 11, "67" : 10, "68" : 9,  "69" : 8,  "70" : 7,  "71" : 6, 
+    "72" : 5,  "73" : 4,  "74" : 3,  "75" : 2,  "76" : 1
+    }
 
-def extract_single_order(imhdu, wavhdu, orderfilter, ordername, wavbox, wavmask):
+
+# Orders up to 72 are well-separated and work with both methods
+ordermax = 72
+
+def shrink_box(r, h=8.0):
     """
-    Isolate a single order
+    Change the height of a pyregion Box region
     """
-    # All pixels that we think are in this order
-    ordermask = orderfilter.mask(wavhdu.data.shape)
-
-    # First find wavelengths that ought to fall in the order
-    orderwavs = wavhdu.data[ordermask & wavmask]
-    if len(orderwavs):
-        print "{} : {:.2f}-{:.2f}".format(ordername, orderwavs.min(), orderwavs.max())
-    else:
-        print "{} : No valid wavelengths found".format(ordername)
-
-    # Second, look at wavelengths in the contiguous wavelength box that we found
-    orderwavs = wavhdu.data[wavbox]
-    orderwavs = orderwavs[orderwavs > 1000.0]
-    if len(orderwavs):
-        print "{}*: {:.2f}-{:.2f}".format(ordername, orderwavs.min(), orderwavs.max())
-    else:
-        print "{}*: No valid wavelengths found".format(ordername)
-
-    # enclosing rectangle around this order
-    bbox, = ndi.find_objects(ordermask.astype(int))
-    print bbox, wavbox
-
-    # 
-    return imhdu.data[bbox]
-
-
+    r.coord_list[3] = h
+    return r
 
 
 def extract_orders(specfile, wavfile, regionfile,
@@ -46,7 +33,12 @@ def extract_orders(specfile, wavfile, regionfile,
     imhdu, = pyfits.open(specfile + ".fits")
     wavhdu, = pyfits.open(wavfile + ".fits")
     regions = pyregion.open(regionfile + ".reg")
+    wide_filters = regions.get_filter()
+
+    # Restrict attention to the center of each slit
+    regions = pyregion.ShapeList([shrink_box(region) for region in regions])
     filters = regions.get_filter()
+    
     ordernames = [box.attr[1]["text"] for box in regions]
 
     #
@@ -56,14 +48,60 @@ def extract_orders(specfile, wavfile, regionfile,
     # All pixels that have a valid wavelength
     wavmask = (wavhdu.data >= wavmin) & (wavhdu.data <= wavmax)
     labels, nlabels = ndi.label(wavmask, structure=np.ones((3,3)))
-    wavboxes = ndi.find_objects(labels)
-    wavboxes.reverse()
-    
-    for orderfilter, ordername, wavbox in zip(filters, ordernames, wavboxes):
-        extract_single_order(imhdu, wavhdu, orderfilter, ordername, wavbox, wavmask)
 
+    # save a copy of the labels for debugging
+    pyfits.PrimaryHDU(labels).writeto("orders-labels.fits", clobber=True)
 
+    print "Number of order boxes found: ", len(ordernames)
+    print "Number of objects found: ", nlabels
 
+    for widefilter, orderfilter, ordername in zip(wide_filters, filters, ordernames):
+        # All pixels that we think are in the central part of the slit in this order
+        ordermask = orderfilter.mask(wavhdu.data.shape)
+        # and the same for the entire order
+        widemask = widefilter.mask(wavhdu.data.shape)
+        
+
+        # First find wavelengths that ought to fall in the order
+        orderwavs = wavhdu.data[ordermask & wavmask]
+        if len(orderwavs):
+            print "{} : {:.2f}-{:.2f}".format(ordername, orderwavs.min(), orderwavs.max())
+        else:
+            print "{} : No valid wavelengths found".format(ordername)
+
+        # Second, look at wavelengths in the contiguous wavelength box that we found
+        label = box2label[ordername.split()[-1]]
+        iorder = int(ordername.split()[-1])
+        orderwavs = wavhdu.data[labels == label]
+        if len(orderwavs):
+            print "Label {}: {:.2f}-{:.2f}".format(label, orderwavs.min(), orderwavs.max())
+        else:
+            print "{}*: No valid wavelengths found".format(ordername)
+
+        print
+
+        # enclosing rectangle around this entire order
+        bbox, = ndi.find_objects(widemask.astype(int))
+        
+        imorder = imhdu.data[bbox]
+        wavorder = wavhdu.data[bbox]
+        # Construct a mask of all pixels both methods say should be in this order
+        if (iorder <= ordermax):
+            # These orders are the easiest to deal with
+            m = widemask & (labels == label)
+        else:
+            m = labels == label
+        m = m[bbox]
+        print "Number of wavelength pixels found: ", np.sum(m)
+        mm = widemask[bbox] # less stringent mask
+        ny, nx = wavorder.shape
+        # Use a single average wavelength for each column
+        meanwav = np.sum(wavorder*m, axis=0) / m.sum(axis=0)
+        meanwav = np.vstack( [meanwav]*ny )
+        pyfits.PrimaryHDU(imorder*mm).writeto("test-order{}-im.fits".format(iorder), 
+                                           clobber=True)
+        pyfits.PrimaryHDU(meanwav).writeto("test-order{}-wav.fits".format(iorder), 
+                                           clobber=True)
 
 
 if __name__ == "__main__":
