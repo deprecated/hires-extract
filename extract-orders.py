cb3d8e357bcffb6f5d03ea61d3473306b085db1c
William Henney <will@henney.org>
2013-02-20 21:55:40 -0600

    Remove the linear horizontal tilt from the orders
    There is still a small quadratic term, expecially noticable in the red.

--- extract-orders.py
+++ extract-orders.py
@@ -3,6 +3,8 @@ import astropy.io.fits as pyfits
 import pyregion
 import argparse
 import scipy.ndimage as ndi
+import skimage.morphology
+
 
 # Mapping between the boxes that we added by hand and the labels of the contiguous patches
 box2label = {
@@ -23,6 +25,17 @@ def shrink_box(r, h=8.0):
     r.coord_list[3] = h
     return r
 
+def dilate_mask(mask, margin=1):
+    """
+    Add an extra margin (default 1 pixel) around all true areas of a logical mask
+    """
+    return skimage.morphology.dilation(
+               mask.astype(np.uint8), 
+               skimage.morphology.square(2*margin + 1)
+               ).astype(bool)
+
+
+
 
 def extract_orders(specfile, wavfile, regionfile,
                    wavmin=1000.0, wavmax=10000.0):
@@ -33,6 +46,10 @@ def extract_orders(specfile, wavfile, regionfile,
     imhdu, = pyfits.open(specfile + ".fits")
     wavhdu, = pyfits.open(wavfile + ".fits")
     regions = pyregion.open(regionfile + ".reg")
+
+    # Tilt angles from the horizontal
+    tilts = [r.coord_list[4] for r in regions]
+
     wide_filters = regions.get_filter()
 
     # Restrict attention to the center of each slit
@@ -55,11 +72,11 @@ def extract_orders(specfile, wavfile, regionfile,
     print "Number of order boxes found: ", len(ordernames)
     print "Number of objects found: ", nlabels
 
-    for widefilter, orderfilter, ordername in zip(wide_filters, filters, ordernames):
+    for widefilter, orderfilter, ordername, tilt in zip(wide_filters, filters, ordernames, tilts):
         # All pixels that we think are in the central part of the slit in this order
         ordermask = orderfilter.mask(wavhdu.data.shape)
-        # and the same for the entire order
-        widemask = widefilter.mask(wavhdu.data.shape)
+        # and the same for the entire order, but adding some padding
+        widemask = dilate_mask(widefilter.mask(wavhdu.data.shape), 3)
         
 
         # First find wavelengths that ought to fall in the order
@@ -83,8 +100,8 @@ def extract_orders(specfile, wavfile, regionfile,
         # enclosing rectangle around this entire order
         bbox, = ndi.find_objects(widemask.astype(int))
         
-        imorder = imhdu.data[bbox]
-        wavorder = wavhdu.data[bbox]
+        imorder = imhdu.data.copy()[bbox]
+        wavorder = wavhdu.data.copy()[bbox]
         # Construct a mask of all pixels both methods say should be in this order
         if (iorder <= ordermax):
             # These orders are the easiest to deal with
@@ -92,13 +109,32 @@ def extract_orders(specfile, wavfile, regionfile,
         else:
             m = labels == label
         m = m[bbox]
-        print "Number of wavelength pixels found: ", np.sum(m)
+        print "Number of good wavelength pixels found in order box: ", np.sum(m)
         mm = widemask[bbox] # less stringent mask
+
+        #
+        # Remove the horizontal tilt of the orders
+        #
         ny, nx = wavorder.shape
+        jshifts = (np.arange(nx)*np.tan(np.radians(tilt))).astype(int) # required shift of each column
+        jtrim = jshifts.max() # Amount to trim off the top of the strip at the end
+        jshifts = np.vstack( [jshifts]*ny ) # Expand back to 2D
+        for chunk in ndi.find_objects(jshifts): # Process in chunks that have the same jshift
+            jshift = jshifts[chunk][0,0]        # How much to shift this chunk
+            # apply the shift to all the arrays
+            imorder[chunk] = np.roll(imorder[chunk], -jshift, axis=0)
+            wavorder[chunk] = np.roll(wavorder[chunk], -jshift, axis=0)
+            m[chunk] = np.roll(m[chunk], -jshift, axis=0)
+            mm[chunk] = np.roll(mm[chunk], -jshift, axis=0)
+            
         # Use a single average wavelength for each column
         meanwav = np.sum(wavorder*m, axis=0) / m.sum(axis=0)
         meanwav = np.vstack( [meanwav]*ny )
-        pyfits.PrimaryHDU(imorder*mm).writeto("test-order{}-im.fits".format(iorder), 
+        # Trim the useless space off the top
+        imorder = imorder[:-jtrim,:]
+        meanwav = meanwav[:-jtrim,:]
+        # And save each order to FITS files
+        pyfits.PrimaryHDU(imorder).writeto("test-order{}-im.fits".format(iorder), 
                                            clobber=True)
         pyfits.PrimaryHDU(meanwav).writeto("test-order{}-wav.fits".format(iorder), 
                                            clobber=True)
