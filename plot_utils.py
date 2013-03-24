925d8cd77a28fd429f3872992eb9dc7aa0d190db
William Henney <will@henney.org>
2013-03-23 18:12:50 -0600

    Improve the moment plots
    * plot_utils.py:  Additional lines showing the intensities and
      velocities of the 2-delta decompositions of the line profile.

--- plot_utils.py
+++ plot_utils.py
@@ -8,11 +8,42 @@ mom_names = ["intensity", "mean", "sigma", "skewness"]
 def plot_moments(prefix):
     data = json.load(open(prefix + ".json"))
     y = data["position"]
+    # Intensities
+    plt.subplot(411)
     plt.title("Velocity moments")
-    for i, m in enumerate(mom_names, start=1):
-        plt.subplot(len(mom_names), 1, i)
-        plt.plot(y, data[m])
-        plt.ylabel(m)
+    plt.plot(y, data["intensity"], label='total')
+    plt.plot(y, data["I1"], label='1')
+    plt.plot(y, data["I2"], label='2')
+    plt.ylim(0.0, None)
+    plt.ylabel("Intensity")
+    plt.grid()
+    plt.legend(fontsize="x-small")
+    plt.setp(plt.gca().get_xticklabels(), visible=False)
+
+    # Velocities
+    plt.subplot(412)
+    plt.plot(y, data["mean"], label='mean')
+    plt.plot(y, data["v1"], label='1')
+    plt.plot(y, data["v2"], label='2')
+    plt.ylabel("Velocity")
+    plt.grid()
+    plt.legend(fontsize="x-small")
+    plt.setp(plt.gca().get_xticklabels(), visible=False)
+
+    # Sigma
+    plt.subplot(413)
+    plt.plot(y, data["sigma"])
+    plt.ylabel("Sigma")
+    plt.grid()
+    plt.ylim(0.0, None)
+    plt.setp(plt.gca().get_xticklabels(), visible=False)
+
+    # Skewness
+    plt.subplot(414)
+    plt.plot(y, data["skewness"])
+    plt.ylabel("Skew")
+    plt.grid()
+
     plt.xlabel("Position along slit, arcsec")
     plt.savefig(prefix + "-plot.pdf")
 
