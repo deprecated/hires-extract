e6886187eb82eabe3d16ab6b4cb5a88c36a90c45
William Henney <will@henney.org>
2013-03-28 12:55:17 -0600

    Port previous improvements to doublet plots

--- image_utils.py
+++ image_utils.py
@@ -20,12 +20,13 @@ def arcsec2ypix(hdr, y, u=0.0):
     return pywcs.WCS(hdr).wcs_sky2pix(coords, 0)[:, 1]
 
 
-def doublet_stages(sname, dname, stampdir="Stamps", drange=None):
+def doublet_stages(sname, linename, fancyname=None,
+                   stampdir="Stamps", drange=None):
     """
     Plot all the stages in the reduction of a doublet
     """
     stampname = os.path.join(stampdir,
-                             "-".join([sname, dname, "stamp"]))
+                             "-".join([sname, linename, "stamp"]))
     fig = plt.figure(figsize=[16, 7])
     h = None
     for suff, index, win, title in [
@@ -40,9 +41,10 @@ def doublet_stages(sname, dname, stampdir="Stamps", drange=None):
             h = hdu.header
         d = hdu.data
         ax = pywcsgrid2.subplot(win, header=h)
-        ax.imshow(d, vmin=drange[0], vmax=drange[1],
-                  origin="low", cmap=plt.cm.gray_r,
-                  interpolation="nearest")
+        im = ax.imshow(d, vmin=drange[0], vmax=drange[1],
+                       origin="low", cmap=plt.cm.gray_r,
+                       aspect="auto",
+                       interpolation="nearest")
         ax.set_ticklabel2_type("arcsec", scale=1./3600, nbins=8)
         ax.set_ticklabel1_type("absval", scale=1./1000, nbins=7)
         ax.set_xlim(*kms2xpix(h, [-30.0, 60.0]))
@@ -51,7 +53,24 @@ def doublet_stages(sname, dname, stampdir="Stamps", drange=None):
         ax.axis["bottom"].label.set_text(
             r"$V_{\mathrm{top}}\ \mathrm{(km\,s^{-1})}$")
 
-    plt.tight_layout()
+    # Make a separate axis on the second row just for the line label
+    # Use 5 times as many horizontal divisions
+    ax = plt.subplot(2, 40, 41, frameon=False)
+    ax.axis("off")
+    ax.text(0.0, 0.5, fancyname or linename,
+            fontsize="x-large",
+            horizontalalignment='left',
+            verticalalignment='center',
+            transform=ax.transAxes)
+
+    # And another one for the color bar
+    cb_ax = plt.subplot(2, 40, 46)
+    cb = plt.colorbar(im, cax=cb_ax, orientation="vertical")
+    cb.set_label("Intensity")
+
+    fig.subplots_adjust(bottom=0.08, top=0.95,
+                        left=0.05, right=0.99,
+                        wspace=0.25, hspace=0.4)
     fig.savefig(stampname + "-doublet-stages.pdf")
     plt.close(fig)
 
@@ -113,18 +132,19 @@ def line_stages(sname, linename, fancyname=None,
 
 if __name__ == "__main__":
     # Example plots
-    # doublet_stages("p84", "O_I_6046", drange=[-3.0, 180.0])
-    # doublet_stages("p84", "O_I_7002", drange=[-3.0, 180.0])
-    line_stages("p84", "N_I_5198", "[N I] 5198", drange=[-2.0, 140.0], sky=1)
-    line_stages("p84", "O_I_6300", "[O I] 6300", drange=[-10.0, 1600.0], sky=1)
-    line_stages("p84", "C_II_6578", "C II 6578", drange=[-2.0, 150.0])
-    line_stages("p84", "He_I_S_6678", "He I 6678", drange=[-5.0, 700.0])
-    line_stages("p84", "O_I_5577", "[O I] 5577", drange=[-3.0, 250.0], sky=1)
-    line_stages("p84", "Si_II_6371", "Si II 6371", drange=[-3.0, 120.0])
-    line_stages("p84", "O_III_5007", "[O III] 5007", drange=[-10.0, 10000.0])
-    line_stages("p84", "S_II_6731", "[S II] 6731", drange=[-10.0, 5000.0])
-    line_stages("p84", "N_II_6548", "[N II] 6548", drange=[-10.0, 10000.0])
-    line_stages("p84", "Cl_III_5538", "[Cl III] 5538", drange=[-2.0, 100.0])
-    line_stages("p84", "Fe_III_4881", "[Fe III] 4881", drange=[-2.0, 60.0])
-    line_stages("p84", "Fe_II_5262", "[Fe II] 5262", drange=[-2.0, 25.0])
-    line_stages("p84", "S_III_6312", "[S III] 6312", drange=[-4.0, 330.0])
+    doublet_stages("p84", "O_I_6046", "O I 6046", drange=[-3.0, 180.0])
+    doublet_stages("p84", "O_I_7002", "O I 7002", drange=[-3.0, 180.0])
+    # line_stages("p84", "N_I_5198", "[N I] 5198", drange=[-2.0, 140.0], sky=1)
+    # line_stages("p84", "O_I_6300", "[O I] 6300",
+    #             drange=[-10.0, 1600.0], sky=1)
+    # line_stages("p84", "C_II_6578", "C II 6578", drange=[-2.0, 150.0])
+    # line_stages("p84", "He_I_S_6678", "He I 6678", drange=[-5.0, 700.0])
+    # line_stages("p84", "O_I_5577", "[O I] 5577", drange=[-3.0, 250.0], sky=1)
+    # line_stages("p84", "Si_II_6371", "Si II 6371", drange=[-3.0, 120.0])
+    # line_stages("p84", "O_III_5007", "[O III] 5007", drange=[-10.0, 10000.0])
+    # line_stages("p84", "S_II_6731", "[S II] 6731", drange=[-10.0, 5000.0])
+    # line_stages("p84", "N_II_6548", "[N II] 6548", drange=[-10.0, 10000.0])
+    # line_stages("p84", "Cl_III_5538", "[Cl III] 5538", drange=[-2.0, 100.0])
+    # line_stages("p84", "Fe_III_4881", "[Fe III] 4881", drange=[-2.0, 60.0])
+    # line_stages("p84", "Fe_II_5262", "[Fe II] 5262", drange=[-2.0, 25.0])
+    # line_stages("p84", "S_III_6312", "[S III] 6312", drange=[-4.0, 330.0])
