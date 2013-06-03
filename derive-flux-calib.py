c3841bdeafc294666911be3986deaef6a0abe358
William Henney <will@henney.org>
2013-06-02 21:28:51 -0500

    Do graph of fit for flux calibration
    We mask out the lines as best we can - using a wider band for the
    stronger lines, but the fit still isn't perfect.
    
    This is mainly because a polynomial is not very appropriate.  I will
    switch to a Gaussian plus polynomial.
    
    Unfortunately, this means switching to lmfit.

--- derive-flux-calib.py
+++ derive-flux-calib.py
@@ -4,6 +4,7 @@ import astropy.io.fits as pyfits
 import lmfit
 import yaml
 import argparse
+import matplotlib.pyplot as plt
 
 ORDERMIN, ORDERMAX = 51, 76
 # HST 1 slit 
@@ -16,13 +17,19 @@ F7000 = 28.0
 J1 = 15
 J2 = 47
 # How wide a range to mask out around each line
-DWAV = 5.0
+DWAV = 3.0
+DWAV_STRONG = 15.0
 
-           
+STRONG_LINES = [
+    "H a 6563", "H b 4861", "[N II] 6583", "[N II] 6548",
+    "[O III] 4959", "[O III] 5007", "[S III] 6312", "[O I] 6300", 
+    "[S II] 6731", "[S II] 6716", "He I S 6678", "He I T 5876",
+]
 
 def main(npoly):
     orders = range(ORDERMIN, ORDERMAX)
     linedb = yaml.load(open("Stamps/line-database.json"))
+    plt.plot([4700.0, 7020.0], [1.0, 1.0], "k", lw=5, alpha=0.3)
     for i in orders:
         f = pyfits.open("Extract/{}-order{}.fits".format(OBJ, i))
         spec = f["SCI"].data[J1:J2,:].mean(axis=0) 
@@ -32,10 +39,15 @@ def main(npoly):
         # Mask out NaNs
         mask = np.isfinite(wav) & np.isfinite(ratio)
         # Mask out lines
-        linewavs = [v['wav'] for v in linedb.values()
+        linewavs = [v['wav'] for k, v in linedb.items()
                     if v['iorder'] == i]
+        strongwavs = [v['wav'] for k, v in linedb.items()
+                      if v['iorder'] == i and k in STRONG_LINES]
         for wav0 in linewavs:
             inthisline = np.abs(wav-wav0) < 0.5*DWAV
+            mask[inthisline] = False
+        for wav0 in strongwavs:
+            inthisline = np.abs(wav-wav0) < 0.5*DWAV_STRONG
             mask[inthisline] = False
         # And just to be on the safe side, mask out outliers too
         outliers = np.abs(ratio - 1.0) > 0.75 
@@ -53,7 +65,23 @@ def main(npoly):
             print coeffs
         except:
             print "polyfit failed"
+            continue
         
+        plt.plot(wav, ratio, "-", alpha=0.7, lw=0.3)
+        plt.plot(wav, p(x), "-k", alpha=0.4, lw=2)
+        meanwav = wav[mask].mean()
+        plt.text(meanwav, 0.2, "{}".format(i), horizontalalignment='center')
+
+    F = plt.gcf()
+    F.set_size_inches( (30, 3) )
+    plt.grid()
+    plt.xlim(4700., 7020)
+    plt.ylim(0.0, 2.1)
+    plt.xlabel("Wavelength, Angstrom")
+    plt.ylabel("F(lam) / lam^{:.2f}".format(M))
+    pltname = "derive-flux-calib.png"
+    plt.savefig(pltname, bbox_inches='tight', dpi=300)
+
 
 
 if __name__ == "__main__":
